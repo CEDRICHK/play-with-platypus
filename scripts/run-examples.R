eefb411c254617f40ff05bce9044021de2bdb723
CEDRICHK <cedric.hassen-khodja@mri.cnrs.fr>
2020-10-13 18:16:02 +0200

    edit script to run examples

---
+++ scripts/run-examples.R
@@ -0,0 +1,10 @@
+
+# install platypus --------------------------------------------------------
+remotes::install_github("maju116/platypus")
+
+# Load packages -----------------------------------------------------------
+library(tidyverse)
+library(platypus)
+library(abind)
+
+# Build U-Net -------------------------------------------------------------
